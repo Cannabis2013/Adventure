b8133968111fe5bdc5faa935818ca472216895f9
Stefan <stef240p@stud.kea.dk>
2022-03-28 13:31:39 +0200

    Added weapon stats to weapon prints

--- src/GameEngine/MapGeneration/SmallSquare/InitializeMap/MapItems/Item.java
+++ src/GameEngine/MapGeneration/SmallSquare/InitializeMap/MapItems/Item.java
@@ -14,5 +14,7 @@ public abstract class Item extends MapObject {
         _itemType = typeDescription;
     }
 
-    public String presentate() {return _title;};
+    //public String presentate() {return _title;};
+    public String presentate() {return _title;}
+
 }
